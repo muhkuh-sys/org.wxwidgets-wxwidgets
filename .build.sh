8b70ff968a4edc897d32350984913f251d89bd75
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2015-03-30 17:13:07 +0200

    Split the build process into 32 and 64 bit parts.

--- .build.sh
+++ .build.sh
@@ -19,126 +19,128 @@ fi
 #
 # Build wxWidgets for windows x86.
 #
-mkdir -p build_win32
-cd build_win32
-export LDFLAGS="-static-libgcc -static-libstdc++"
-../configure --host=i686-w64-mingw32 --prefix=/tmp/wxwidgets/windows_x86/ --enable-vendor=muhkuh --disable-shared --disable-precomp-headers
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
+if [ $MY_TARGET_BITS -eq 32 ]; then
+	mkdir -p build_win32
+	cd build_win32
+	export LDFLAGS="-static-libgcc -static-libstdc++"
+	../configure --host=i686-w64-mingw32 --prefix=/tmp/wxwidgets/windows_x86/ --enable-vendor=muhkuh --disable-shared --disable-precomp-headers
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	
+	make
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	
+	make install
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	
+	pushd /tmp/wxwidgets/windows_x86/lib
+	cp libwx_baseu-3.0-i686-w64-mingw32.a         libwx_baseu-3.0.a
+	cp libwx_baseu_net-3.0-i686-w64-mingw32.a     libwx_baseu_net-3.0.a
+	cp libwx_baseu_xml-3.0-i686-w64-mingw32.a     libwx_baseu_xml-3.0.a
+	cp libwx_mswu_adv-3.0-i686-w64-mingw32.a      libwx_mswu_adv-3.0.a
+	cp libwx_mswu_aui-3.0-i686-w64-mingw32.a      libwx_mswu_aui-3.0.a
+	cp libwx_mswu_core-3.0-i686-w64-mingw32.a     libwx_mswu_core-3.0.a
+	cp libwx_mswu_gl-3.0-i686-w64-mingw32.a       libwx_mswu_gl-3.0.a
+	cp libwx_mswu_html-3.0-i686-w64-mingw32.a     libwx_mswu_html-3.0.a
+	cp libwx_mswu_media-3.0-i686-w64-mingw32.a    libwx_mswu_media-3.0.a
+	cp libwx_mswu_propgrid-3.0-i686-w64-mingw32.a libwx_mswu_propgrid-3.0.a
+	cp libwx_mswu_qa-3.0-i686-w64-mingw32.a       libwx_mswu_qa-3.0.a
+	cp libwx_mswu_ribbon-3.0-i686-w64-mingw32.a   libwx_mswu_ribbon-3.0.a
+	cp libwx_mswu_richtext-3.0-i686-w64-mingw32.a libwx_mswu_richtext-3.0.a
+	cp libwx_mswu_stc-3.0-i686-w64-mingw32.a      libwx_mswu_stc-3.0.a
+	cp libwx_mswu_webview-3.0-i686-w64-mingw32.a  libwx_mswu_webview-3.0.a
+	cp libwx_mswu_xrc-3.0-i686-w64-mingw32.a      libwx_mswu_xrc-3.0.a
+	cp libwxscintilla-3.0-i686-w64-mingw32.a      libwx_mswu_scintilla-3.0.a
+	cp libwxscintilla-3.0-i686-w64-mingw32.a      libwx_mswu_scintilla-3.0-i686-w64-mingw32.a
+	popd
+	
+	pushd /tmp/wxwidgets/windows_x86
+	python $BUILD_ROOT/tests/mingw_dll_dependencies.py bin/wxrc-3.0
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	find . -name *.a -exec python $BUILD_ROOT/tests/mingw_dll_dependencies.py '{}' ';'
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	popd
+	
+	cd ..
 fi
-
-make
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make install
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-pushd /tmp/wxwidgets/windows_x86/lib
-cp libwx_baseu-3.0-i686-w64-mingw32.a         libwx_baseu-3.0.a
-cp libwx_baseu_net-3.0-i686-w64-mingw32.a     libwx_baseu_net-3.0.a
-cp libwx_baseu_xml-3.0-i686-w64-mingw32.a     libwx_baseu_xml-3.0.a
-cp libwx_mswu_adv-3.0-i686-w64-mingw32.a      libwx_mswu_adv-3.0.a
-cp libwx_mswu_aui-3.0-i686-w64-mingw32.a      libwx_mswu_aui-3.0.a
-cp libwx_mswu_core-3.0-i686-w64-mingw32.a     libwx_mswu_core-3.0.a
-cp libwx_mswu_gl-3.0-i686-w64-mingw32.a       libwx_mswu_gl-3.0.a
-cp libwx_mswu_html-3.0-i686-w64-mingw32.a     libwx_mswu_html-3.0.a
-cp libwx_mswu_media-3.0-i686-w64-mingw32.a    libwx_mswu_media-3.0.a
-cp libwx_mswu_propgrid-3.0-i686-w64-mingw32.a libwx_mswu_propgrid-3.0.a
-cp libwx_mswu_qa-3.0-i686-w64-mingw32.a       libwx_mswu_qa-3.0.a
-cp libwx_mswu_ribbon-3.0-i686-w64-mingw32.a   libwx_mswu_ribbon-3.0.a
-cp libwx_mswu_richtext-3.0-i686-w64-mingw32.a libwx_mswu_richtext-3.0.a
-cp libwx_mswu_stc-3.0-i686-w64-mingw32.a      libwx_mswu_stc-3.0.a
-cp libwx_mswu_webview-3.0-i686-w64-mingw32.a  libwx_mswu_webview-3.0.a
-cp libwx_mswu_xrc-3.0-i686-w64-mingw32.a      libwx_mswu_xrc-3.0.a
-cp libwxscintilla-3.0-i686-w64-mingw32.a      libwx_mswu_scintilla-3.0.a
-cp libwxscintilla-3.0-i686-w64-mingw32.a      libwx_mswu_scintilla-3.0-i686-w64-mingw32.a
-popd
-
-pushd /tmp/wxwidgets/windows_x86
-python $BUILD_ROOT/tests/mingw_dll_dependencies.py bin/wxrc-3.0
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-find . -name *.a -exec python $BUILD_ROOT/tests/mingw_dll_dependencies.py '{}' ';'
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-popd
-
-cd ..
-
 
 #-----------------------------------------------------------------------------
 #
 # Build wxWidgets for windows amd64.
 #
-mkdir -p build_win64
-cd build_win64
-export LDFLAGS="-static-libgcc -static-libstdc++"
-../configure --host=x86_64-w64-mingw32 --prefix=/tmp/wxwidgets/windows_amd64/ --enable-vendor=muhkuh --disable-shared --disable-precomp-headers
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	cat config.log
-	exit 1
+if [ $MY_TARGET_BITS -eq 64 ]; then
+	mkdir -p build_win64
+	cd build_win64
+	export LDFLAGS="-static-libgcc -static-libstdc++"
+	../configure --host=x86_64-w64-mingw32 --prefix=/tmp/wxwidgets/windows_amd64/ --enable-vendor=muhkuh --disable-shared --disable-precomp-headers
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		cat config.log
+		exit 1
+	fi
+	
+	make
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	
+	make install
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	
+	pushd /tmp/wxwidgets/windows_amd64/lib
+	cp libwx_baseu-3.0-x86_64-w64-mingw32.a         libwx_baseu-3.0.a
+	cp libwx_baseu_net-3.0-x86_64-w64-mingw32.a     libwx_baseu_net-3.0.a
+	cp libwx_baseu_xml-3.0-x86_64-w64-mingw32.a     libwx_baseu_xml-3.0.a
+	cp libwx_mswu_adv-3.0-x86_64-w64-mingw32.a      libwx_mswu_adv-3.0.a
+	cp libwx_mswu_aui-3.0-x86_64-w64-mingw32.a      libwx_mswu_aui-3.0.a
+	cp libwx_mswu_core-3.0-x86_64-w64-mingw32.a     libwx_mswu_core-3.0.a
+	cp libwx_mswu_gl-3.0-x86_64-w64-mingw32.a       libwx_mswu_gl-3.0.a
+	cp libwx_mswu_html-3.0-x86_64-w64-mingw32.a     libwx_mswu_html-3.0.a
+	cp libwx_mswu_media-3.0-x86_64-w64-mingw32.a    libwx_mswu_media-3.0.a
+	cp libwx_mswu_propgrid-3.0-x86_64-w64-mingw32.a libwx_mswu_propgrid-3.0.a
+	cp libwx_mswu_qa-3.0-x86_64-w64-mingw32.a       libwx_mswu_qa-3.0.a
+	cp libwx_mswu_ribbon-3.0-x86_64-w64-mingw32.a   libwx_mswu_ribbon-3.0.a
+	cp libwx_mswu_richtext-3.0-x86_64-w64-mingw32.a libwx_mswu_richtext-3.0.a
+	cp libwx_mswu_stc-3.0-x86_64-w64-mingw32.a      libwx_mswu_stc-3.0.a
+	cp libwx_mswu_webview-3.0-x86_64-w64-mingw32.a  libwx_mswu_webview-3.0.a
+	cp libwx_mswu_xrc-3.0-x86_64-w64-mingw32.a      libwx_mswu_xrc-3.0.a
+	cp libwxscintilla-3.0-x86_64-w64-mingw32.a      libwx_mswu_scintilla-3.0.a
+	cp libwxscintilla-3.0-x86_64-w64-mingw32.a      libwx_mswu_scintilla-3.0-x86_64-w64-mingw32.a
+	popd
+	
+	pushd /tmp/wxwidgets/windows_amd64
+	python $BUILD_ROOT/tests/mingw_dll_dependencies.py bin/wxrc-3.0
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	find . -name *.a -exec python $BUILD_ROOT/tests/mingw_dll_dependencies.py '{}' ';'
+	STATUS=$?
+	if [ $STATUS -ne 0 ]; then
+		exit 1
+	fi
+	popd
+	
+	cd ../..
 fi
-
-make
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-make install
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-
-pushd /tmp/wxwidgets/windows_amd64/lib
-cp libwx_baseu-3.0-x86_64-w64-mingw32.a         libwx_baseu-3.0.a
-cp libwx_baseu_net-3.0-x86_64-w64-mingw32.a     libwx_baseu_net-3.0.a
-cp libwx_baseu_xml-3.0-x86_64-w64-mingw32.a     libwx_baseu_xml-3.0.a
-cp libwx_mswu_adv-3.0-x86_64-w64-mingw32.a      libwx_mswu_adv-3.0.a
-cp libwx_mswu_aui-3.0-x86_64-w64-mingw32.a      libwx_mswu_aui-3.0.a
-cp libwx_mswu_core-3.0-x86_64-w64-mingw32.a     libwx_mswu_core-3.0.a
-cp libwx_mswu_gl-3.0-x86_64-w64-mingw32.a       libwx_mswu_gl-3.0.a
-cp libwx_mswu_html-3.0-x86_64-w64-mingw32.a     libwx_mswu_html-3.0.a
-cp libwx_mswu_media-3.0-x86_64-w64-mingw32.a    libwx_mswu_media-3.0.a
-cp libwx_mswu_propgrid-3.0-x86_64-w64-mingw32.a libwx_mswu_propgrid-3.0.a
-cp libwx_mswu_qa-3.0-x86_64-w64-mingw32.a       libwx_mswu_qa-3.0.a
-cp libwx_mswu_ribbon-3.0-x86_64-w64-mingw32.a   libwx_mswu_ribbon-3.0.a
-cp libwx_mswu_richtext-3.0-x86_64-w64-mingw32.a libwx_mswu_richtext-3.0.a
-cp libwx_mswu_stc-3.0-x86_64-w64-mingw32.a      libwx_mswu_stc-3.0.a
-cp libwx_mswu_webview-3.0-x86_64-w64-mingw32.a  libwx_mswu_webview-3.0.a
-cp libwx_mswu_xrc-3.0-x86_64-w64-mingw32.a      libwx_mswu_xrc-3.0.a
-cp libwxscintilla-3.0-x86_64-w64-mingw32.a      libwx_mswu_scintilla-3.0.a
-cp libwxscintilla-3.0-x86_64-w64-mingw32.a      libwx_mswu_scintilla-3.0-x86_64-w64-mingw32.a
-popd
-
-pushd /tmp/wxwidgets/windows_amd64
-python $BUILD_ROOT/tests/mingw_dll_dependencies.py bin/wxrc-3.0
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-find . -name *.a -exec python $BUILD_ROOT/tests/mingw_dll_dependencies.py '{}' ';'
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	exit 1
-fi
-popd
-
-cd ../..
-
 
 #-----------------------------------------------------------------------------
 #
@@ -147,7 +149,7 @@ cd ../..
 rm -rf build
 mkdir build
 cd build
-cmake ../installer/ivy
+cmake ../installer/ivy -DMY_TARGET_BITS=$MY_TARGET_BITS
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
         exit 1
